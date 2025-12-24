b2154e4949cbe5d82fd70b68aff1c4407674daad
kimjjcc <164847756+kimjjcc@users.noreply.github.com>
2025-12-25 00:47:23 +0900

    Update LastWarConstructionTime.py

--- LastWarConstructionTime.py
+++ LastWarConstructionTime.py
@@ -231,14 +231,25 @@ BUILDING_DATA = {
 # ----------------------
 # 건물 / 레벨 선택
 # ----------------------
-st.markdown("<p style='font-size:25px; font-weight:bold; margin-bottom:0px;'>🏗️ 건물 선택</p>", unsafe_allow_html=True)
-building = st.selectbox("", BUILDING_DATA.keys(), key="building")
+st.markdown("""
+<style>
+[data-baseweb="select"] { margin-top: 0px !important; margin-bottom: 0px !important; }
+.element-container { padding-bottom: 0rem !important; }
+</style>
+<p style='font-size:25px; font-weight:bold; margin:0px;'>🏗️ 건물 선택</p>
+""", unsafe_allow_html=True)
 
-st.markdown("<p style='font-size:25px; font-weight:bold; margin-bottom:0px; margin-top:10px;'>레벨 구간</p>", unsafe_allow_html=True)
-level = st.selectbox("", list(BUILDING_DATA[building].keys())[::-1], key="level")
+building = st.selectbox("", BUILDING_DATA.keys(), key="building", label_visibility="collapsed")
+
+st.markdown("""
+<p style='font-size:25px; font-weight:bold; margin:0px;'>레벨 구간</p>
+""", unsafe_allow_html=True)
+
+level = st.selectbox("", list(BUILDING_DATA[building].keys())[::-1], key="level", label_visibility="collapsed")
 
 data = BUILDING_DATA[building][level]
 d, h, m, s = data["time"]
+
 
 
 
@@ -291,6 +302,7 @@ if st.button("🚀 계산하기", use_container_width=True):
         st.metric("⚡ 최종 건설 시간", f"{dur.days}D {dur.seconds//3600:02}:{(dur.seconds%3600)//60:02}:{dur.seconds%60:02}")
 
     st.metric("📅 완료 예정 시각", end_time.strftime("%Y-%m-%d %H:%M:%S"))
+
 
 
 
