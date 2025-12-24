53e5569280155f6cdd4e85c67be23d917fa1aab2
kimjjcc <164847756+kimjjcc@users.noreply.github.com>
2025-12-25 00:30:55 +0900

    Update LastWarConstructionTime.py

--- LastWarConstructionTime.py
+++ LastWarConstructionTime.py
@@ -35,7 +35,7 @@ with col2:
     st.markdown(
         """
         <div style='display:flex; align-items:center; height:100%;'>
-            <h2 style='margin:0; padding-left:5px;'>Last War 건설 시간 계산기</h2>
+            <h2 style='margin:0; padding-left:2px;'>Last War 건설 시간 계산기</h2>
         </div>
         """,
         unsafe_allow_html=True
@@ -285,6 +285,7 @@ if st.button("🚀 계산하기", use_container_width=True):
         st.metric("⚡ 최종 건설 시간", f"{dur.days}D {dur.seconds//3600:02}:{(dur.seconds%3600)//60:02}:{dur.seconds%60:02}")
 
     st.metric("📅 완료 예정 시각", end_time.strftime("%Y-%m-%d %H:%M:%S"))
+
 
 
 
